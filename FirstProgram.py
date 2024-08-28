0f332d3c360e5c0fc6ff63de1b26c1abc0bd7129
fredoblous <147002711+fredoblous@users.noreply.github.com>
2024-08-28 16:37:48 +0000

    made a mad lib and a program that gets user's info

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,20 +1,33 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Joshua
+#Date: 8/28/24
+#Assignment: Lab 1
+#Purpose: Asks for the user's name and age and uses the provided information to formulate the birth year of the user
 
 def main():
   print("First Program")
   #Say hello
+  print("Hello User")
   
   #Ask for the user's name
+  name = input("What is your name? ")
 
   #Use the user's name in the program.
+  print("Nice to meet you,", name + ".")
 
   #Ask the user for their age.
-
+  age = int(input("How old are you? "))
   #Tell the user what year they were born in.
+  print("Based on your given input you were born in", 2024 - age)
+
   #Assume that they have not had their birthday yet this year.
+  birth_year = int(input("What year were you born in? "))
+  
+  if birth_year != (2024 - age):
+     print("You have not had your birthday yet.")
+  else:
+     print("You have had your birthday this year")
+
 
 
 #Call the main function if this is the file being run.
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,36 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Joshua
+#Date: 8/28/24
+#Assignment: Lab1
+#Purpose: To create a story using the user's inputs
 
 def main():
   print("Madlib")
   #Ask user for words
+  noun_one = input("Enter a food item: ")
+  print(noun_one)
+
+  noun_two = input("Enter a person: ")
+  print(noun_two)
+
+  noun_three = input("Enter a place: ")
+  print(noun_three)
+
+  adjective_one = input("Enter a adjective that relates to size: ")
+  print(adjective_one)
+
+  adjective_two = input("Enter a adjective that relates to age: ")
+  print(adjective_two)
+
+  adjective_three = input("Enter a adjective that relates to smell: ")
+  print(adjective_three)
+
+  verb_one = input("Enter a verb: ")
+  print(verb_one)
+
 
   #Print the story with the user supplied words.
-
+  print(f"{adjective_three} {noun_two} likes to {verb_one} {adjective_two} {noun_one} at {adjective_one} {noun_three}.")
 
 
 #Call the main function if this is the file being run.
